7c6760656d4dc8a678e385810ecaab42d788a43d
jagdishmalik <jagdishmalik27x4@gmail.com>
2023-02-18 01:00:57 +0530

    add edit new branch

--- myscript.js
+++ myscript.js
@@ -10,3 +10,9 @@ console.log(x)
 //git branch -m main
 //gut remote add origin "repositary address copy from github website"
 //git push -u origin main
+//if again need to push file to gihub follow below process
+//git add
+// git commit -m "put your comment of change "
+//git branch -m jagdish
+//gut remote add origin "repositary address copy from github website"
+//git push -u origin main
