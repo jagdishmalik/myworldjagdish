b127ec55fd0ae6b975096ec2a197439b7010243f
jagdishmalik <jagdishmalik27x4@gmail.com>
2023-02-18 00:49:47 +0530

    add process of push from terminal

--- myscript.js
+++ myscript.js
@@ -4,3 +4,9 @@ function run(a,b){
 
 let x= run(34,80);
 console.log(x)
+// git init
+//git add
+// git commit -m "put your comment of change "
+//git branch -m main
+//gut remote add origin "repositary address copy from github website"
+//git push -u origin main
